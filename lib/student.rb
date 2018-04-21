40faa7bad4db0d34886d25096e3810ce62bc71f4
Jeamay Palo <jeamay.palo@gmail.com>
2018-04-21 19:12:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,17 @@ class Student
         self.new_from_db(row)
       end
   end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade IS NOT 12
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+  end
 
   
   def save
